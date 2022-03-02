b8bfc48fa5cc4876ce169b8468b88ecaa7b0f849
Rizvan Gataev <Rizvan8103@gmail.com>
2022-03-02 14:04:53 +0200

    Console program (bonus) task
    Add Console program (bonus) task

---
+++ Konsoliohjelma (Bonus)/Program.cs
@@ -0,0 +1,43 @@
+﻿using CommandLine;
+
+namespace Konsoliohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (o.Test == false)
+                    {
+                    Console.WriteLine("O.Test equals " + o.Test);
+                        Console.WriteLine("Incorrect");
+                    }
+                    else
+                    {
+                        Console.WriteLine("OK");
+                    }
+
+                    if (o.Verbose == true)
+                    {
+                    Console.WriteLine("O.Verbose equals " + o.Verbose);
+                        Console.WriteLine("Correct");
+                    }
+
+                });
+        }
+    }
+
+    class Options
+    {
+        [Option('t', "test")]
+        public bool Test { get; set; }
+
+        [Option('v', "verbose")]
+        public bool Verbose { get; set; }
+
+        [Option('u', "url")]
+        public string? Url { get; set; }
+    }
+}
